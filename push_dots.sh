3053a77a5aafffd9efacbdc85c85d5a93161dbf1
ludvary <rskrishnadasnair@gmail.com>
2023-07-13 09:48:16 +0530

    added stuff

--- push_dots.sh
+++ push_dots.sh
@@ -8,19 +8,13 @@
 cp ~/.zshrc /media/HOME/hause/significant\ things/dot_files/ && echo .zshrc was copied successfully!
 
 # get .config
-rsync -avz --exclude-from=$HOME/scripts/exclude_from_config.txt ~/.config /media/HOME/hause/significant\ things/dot_files
-
-echo ==================================
-echo
-
-# copy the .config exclude list
-cp ~/scripts/exclude_from_config.txt /media/HOME/hause/significant\ things/dot_files && echo exclude list was copied successfully!
+rsync -avz --exclude-from=$HOME/scripts/exclude_from_config.txt ~/.config /media/HOME/hause/significant\ things/dot_files && echo .config was copied successfully!
 
 echo ==================================
 echo
 
 # get scripts
-rsync -avz ~/scripts /media/HOME/hause/significant\ things/dot_files
+rsync -avz ~/scripts /media/HOME/hause/significant\ things/dot_files && echo scripts were coped successfully!
 
 echo ==================================
 echo
