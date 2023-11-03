e4b301814d6ee564a7bfc15be2b2db19e348b046
ludvary <rskrishnadasnair@gmail.com>
2023-11-03 13:33:11 +0530

    added stuff

--- scripts/push_dots.sh
+++ scripts/push_dots.sh
@@ -4,31 +4,31 @@
 cp ~/.zshrc /media/HOME/hause/significant\ things/dot_files/ && echo .zshrc was copied successfully!
 
 # get .config
-rsync -avz --exclude-from=$HOME/scripts/exclude_from_config.txt ~/.config /media/HOME/hause/significant\ things/dot_files && echo .config was copied successfully!
+rsync -avz --exclude-from=$HOME/scripts/exclude_from_config.txt ~/.config $HOME/hause/significant\ things/dot_files && echo .config was copied successfully!
 
 echo ==================================
 echo
 
 # get scripts
-rsync -avz ~/scripts /media/HOME/hause/significant\ things/dot_files && echo scripts were coped successfully!
+rsync -avz ~/scripts $HOME/hause/significant\ things/dot_files && echo scripts were coped successfully!
 
 echo ==================================
 echo
 
 # copy zsh custom robbyrussel theme
-cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme /media/HOME/hause/significant\ things/dot_files && echo zsh theme copied successfully!!
+cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme $HOME/hause/significant\ things/dot_files && echo zsh theme copied successfully!!
 
 echo ==================================
 echo
 
 # copy yourself
-cp ~/scripts/push_dots.sh /media/HOME/hause/significant\ things/dot_files/ && echo push_dots.sh was copied successfully!
+cp ~/scripts/push_dots.sh $HOME/hause/significant\ things/dot_files/ && echo push_dots.sh was copied successfully!
 
 echo ==================================
 echo
 
 # go to dir
-cd /media/HOME/hause/significant\ things/dot_files/
+cd $HOME/hause/significant\ things/dot_files/
 
 # do git stuff
 git add . && git commit -m"added stuff" && git push
